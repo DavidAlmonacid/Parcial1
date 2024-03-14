ec4ff7192dee8cbb5d4b242ebddf50cc89adb328
DavidAlmonacid <65629741+DavidAlmonacid@users.noreply.github.com>
2024-03-14 06:41:38 -0500

    Update MainActivity2Test.java

--- app/src/test/java/com/example/parcial1/MainActivity2Test.java
+++ app/src/test/java/com/example/parcial1/MainActivity2Test.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 
 public class MainActivity2Test extends TestCase {
     public void testCalcularTotal() {
-        int totalTomate = 500 * 3;
+        int totalTomate = 500 * 4;
         int totalCebolla = 300 * 7;
         int result = MainActivity2.calcularTotal(totalTomate, totalCebolla);
         assertEquals(3600, result);
